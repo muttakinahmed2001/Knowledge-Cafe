04e0aca574c8e5eb50ddf2766a96c4d15bd8d913
unknown <muttakinahmed871@gmail.com>
2023-04-01 22:42:58 +0600

    author information

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Blog.css'
 const Blog = (props) => {
-  const{authorName,id,authorImage,blogCoverImage,publishDate}=props.blog
+  const{authorName,id,blogTitle,authorImage,blogCoverImage,publishDate}=props.blog
     return (
         <div className='blog'>
             <img src={blogCoverImage} alt="" />
-            <div className='author-information'></div>
-         
-        </div>
+              <div className="author-information"> <img src={authorImage} alt="" />
+
+              <div className='author-name'>
+              <h5>{authorName}</h5>
+                    <h6>{publishDate}</h6>
+              </div>
+              </div>
+              <h1>{blogTitle}</h1>
+               
+                
+                    
+                
+             </div>
+        
+        
     );
 };
 
